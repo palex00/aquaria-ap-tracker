d33bc23c48cd2e21e09caf5f3b80464d1c227a64
palex00 <32203971+palex00@users.noreply.github.com>
2024-10-20 02:21:08 +0200

    Adding the "Useful Items" to Autotracking

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -1,6 +1,6 @@
 ITEM_MAPPING = {
-	[698105] = {"Hotsoup", "toggle"}, -- Hotsoup x2
-	[698052] = {"Hotsoup", "toggle"},
+
+	-- Forms 
 	[698121] = {"Bindsong", "toggle"},
 	[698120] = {"Energyform", "toggle"},
 	[698115] = {"LiandLisong", "toggle"},
@@ -10,6 +10,8 @@ ITEM_MAPPING = {
 	[698117] = {"Beastform", "toggle"},
 	[698118] = {"Sunform", "toggle"},
 	[698119] = {"Natureform", "toggle"},
+	
+	-- Turtles
 	[698125] = {"TransturtleVeiltopleft", "toggle"},
 	[698126] = {"TransturtleVeiltopright", "toggle"},
 	[698127] = {"TransturtleOpenWatertopright", "toggle"},
@@ -19,12 +21,25 @@ ITEM_MAPPING = {
 	[698131] = {"TransturtleFinalBoss", "toggle"},
 	[698132] = {"TransturtleSimonSays", "toggle"},
 	[698133] = {"TransturtleArnassiRuins", "toggle"},
+	
+	-- Misc Progression
 	[698007] = {"BabyDumbo", "toggle"},
 	[698021] = {"BabyNautilus", "toggle"},
 	[698022] = {"BabyPiranha", "toggle"},
 	[698005] = {"BabyBlaster", "toggle"},
-	[698014] = {"JellyCostume", "toggle"},
 	[698023] = {"ArnassiArmor", "toggle"},
+	[698105] = {"Hotsoup", "toggle"}, -- Hotsoup x2
+	[698052] = {"Hotsoup", "toggle"},
+	
+	-- Useful
+	[698014] = {"JellyCostume", "toggle"},
 	[698006] = {"CrabArmor", "toggle"},
 	[698099] = {"Wok", "toggle"},
+	[698116] = {"ShieldSong", "toggle"},
+	[698038] = {"ArcanePoultice", "toggle"},
+	[698045] = {"DumboIceCream", "toggle"},
+	[698055] = {"LeafPoultice", "toggle"},
+	[698107] = {"LeafPoultice", "toggle"}, -- Leaf Poultice x3
+	[698085] = {"SpicyRoll", "toggle"},
+	[698037] = {"ZuunasPerogi", "toggle"},
 }
